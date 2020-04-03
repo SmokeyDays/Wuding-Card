69707ec403af9f17f94c8ad44c4d904880420e40
SmokeyDays <fyq61002427@126.com>
2020-04-03 21:37:15 +0800

    品质更新：按钮颜色更新，便于区分其对应的属性。

--- client/src/components/MyCharInfo.jsx
+++ client/src/components/MyCharInfo.jsx
@@ -15,14 +15,14 @@ function MyHealthInfo({
   }
   const wid=charState.myHealth*10;
   return <div className={classNames(styles.MyCharInfo, className)}>
-    <div style={{position: "absolute",bottom: "20px"}}>
+    <div style={{position: "absolute",bottom: "23px"}}>
       <Button.Group>
         <Button className={styles.MyHealthBTN} onClick={() => changeHealth(1)}>+</Button>
         <Button className={styles.MyHealthBTN} onClick={() => changeHealth(-1)}>-</Button>
       </Button.Group>
     </div>
     <div style={{"background-color": "#666666",height: "22px",width: "50%",position: "absolute",bottom: "0px",left: "0px"}}>
-      <div style={{"background-color": "#FF0000",width: wid+"%",height: "inherit",position: "absolute",right: "0px",bottom: "0px","text-align": "right"}} className="iconfont icon-fire"></div>
+      <div className={styles.bar} style={{"background-color": "#FF0000",width: wid+"%",height: "inherit",position: "absolute",right: "0px",bottom: "0px","text-align": "right"}} className="iconfont icon-fire"></div>
       <p style={{"z-index": "10000",position: "absolute","color": "#FFFFFF"}}>命火: {charState.myHealth || '0'}</p>
     </div>
 
@@ -39,14 +39,14 @@ function MyLevelInfo({
   }
   const wid=charState.myLevel*10;
   return <div className={classNames(styles.MyCharInfo, className)}>
-    <div style={{position: "absolute",bottom: "55px",right: "0px"}}>
+    <div style={{position: "absolute",bottom: "57px",right: "0px"}}>
       <Button.Group>
-        <Button onClick={() => changeLevel(1)}>+</Button>
-        <Button onClick={() => changeLevel(-1)}>-</Button>
+        <Button className={styles.MyLevelBTN} onClick={() => changeLevel(1)}>+</Button>
+        <Button className={styles.MyLevelBTN} onClick={() => changeLevel(-1)}>-</Button>
       </Button.Group>
     </div>
     <div style={{"background-color": "#666666",height: "11px",width: "50%",position: "absolute",top: "0px",right: "0px"}}>
-      <div style={{"background-color": "#0000C6",width: wid+"%",height: "inherit",position: "absolute",left: "0px",bottom: "0px","text-align": "left"}}></div>
+      <div className={styles.bar} style={{"background-color": "#0000C6",width: wid+"%",height: "inherit",position: "absolute",left: "0px",bottom: "0px","text-align": "left"}}></div>
       <p style={{"z-index": "10000",position: "absolute",bottom: "-10px",right: "0px","color": "#FFFFFF","font-size": "9px"}}>修为: {charState.myLevel || '0'}</p>
     </div>
 
@@ -63,14 +63,14 @@ function MyManaInfo({
   }
   const wid=charState.myMana*10;
   return <div className={classNames(styles.MyCharInfo, className)}>
-    <div style={{position: "absolute",bottom: "20px",right: "0px"}}>
+    <div style={{position: "absolute",bottom: "23px",right: "0px"}}>
       <Button.Group>
-        <Button onClick={() => changeMana(1)}>+</Button>
-        <Button onClick={() => changeMana(-1)}>-</Button>
+        <Button className={styles.MyManaBTN} onClick={() => changeMana(1)}>+</Button>
+        <Button className={styles.MyManaBTN} onClick={() => changeMana(-1)}>-</Button>
       </Button.Group>
     </div>
     <div style={{"background-color": "#666666",height: "11px",width: "50%",position: "absolute",bottom: "0px",right: "0px"}}>
-      <div style={{"background-color": "#00FFFF",width: wid+"%",height: "inherit",position: "absolute",left: "0px",bottom: "0px","text-align": "left"}}></div>
+      <div className={styles.bar} style={{"background-color": "#00FFFF",width: wid+"%",height: "inherit",position: "absolute",left: "0px",bottom: "0px","text-align": "left"}}></div>
       <p style={{"z-index": "10000",position: "absolute",bottom: "-10px",right: "0px","color": "#FFFFFF","font-size": "9px"}}>灵力: {charState.myMana || '0'}</p>
     </div>
 
